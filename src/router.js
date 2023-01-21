641c37b029d8b157483c299833db20218b7752b3
Cristiane Maragno <cristianerodriguesmaragno@gmail.com>
2023-01-21 20:11:23 -0300

    Update router mode

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HomePage from '@/views/homePage/homePage.vue'
 import LandingPage from '@/views/landingPage/landingPage.vue'
@@ -32,7 +32,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: routes
 });
 
